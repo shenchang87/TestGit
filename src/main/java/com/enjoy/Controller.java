15937fcbbe8643ed4cebe590549c634bbb21a3cd
shenchang97 <846305275@qq.com>
2019-04-05 12:46:39 +0800

    1

--- src/main/java/com/enjoy/Controller.java
+++ src/main/java/com/enjoy/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 	@RequestMapping("/api/serach")
      public String     saveObj() {
-         String msg= "Hello  word";
+         String msg= "Hello  word   task  01";
          System.out.println("返回数据"+msg);
          return   msg;
      }
